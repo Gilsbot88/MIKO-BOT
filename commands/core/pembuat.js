26fa177137c056cc7b3039ba432161666d2f908a
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-20 15:01:46 +0700

    Update pembuat.js

--- commands/core/pembuat.js
+++ commands/core/pembuat.js
@@ -11,7 +11,7 @@ utilisation: userbot.prefix + "creator",
 async execute(m) {
 let { conn } = data
    conarray = []
-ownerContact = ['6281325994066','6282328303332', '62822980698995','6285783417029','62823283033323','6285640020165','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0']
+ownerContact = ['6283136890848','6283136890848', '6283136890848','6283136890848','6283136890848','6283136890848','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0']
   for (let i of ownerContact.map(v => v + '@s.whatsapp.net')) {
  vname = conn.contacts[i] != undefined ? conn.contacts[i].vname || conn.contacts[i].notify : undefined
   conarray.push({
